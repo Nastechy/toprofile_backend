26bfc8085ec6b00f1862e9cfd2c550f2cb9fe363
Brisstone <okolijohnson69@gmail.com>
2025-09-13 15:33:59 +0100

    push

--- toprofile/settings.py
+++ toprofile/settings.py
@@ -17,6 +17,9 @@ from datetime import timedelta
 import os
 import dj_database_url
 
+import pymysql
+pymysql.install_as_MySQLdb()
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
